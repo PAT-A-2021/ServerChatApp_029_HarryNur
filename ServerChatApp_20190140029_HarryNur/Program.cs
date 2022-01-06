8a83ea1dfa44b16da0c3d3787510327446da107f
Harry Nur <harry.nur.ft19@mail.umy.ac.id>
2022-01-06 17:17:02 +0700

    Add project files.

---
+++ ServerChatApp_20190140029_HarryNur/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_20190140029;
+
+namespace ServerChatApp_20190140029_HarryNur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("I got the need for speed.");
+                Console.ReadLine();
+
+                hostObjek.Close();
+
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
